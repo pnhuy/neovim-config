e8fc010169a242712658d0b8d53f7e95f24adaef
Huy Pham <keixi512@gmail.com>
2023-08-22 03:36:03 +0700

    Add flutter path config

--- after/ftplugin/dart.lua
+++ after/ftplugin/dart.lua
@@ -3,3 +3,7 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.autoindent = true
 vim.opt_local.expandtab = true
 vim.opt_local.smartindent = true
+
+require("flutter-tools").setup {
+    flutter_lookup_cmd = "dirname $(which flutter)",
+}
