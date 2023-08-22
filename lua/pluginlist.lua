fb223eb258890600531171333e7935645ba41092
Huy Pham <keixi512@gmail.com>
2023-08-22 17:29:49 +0700

    Add dashboard plugin

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -195,5 +195,14 @@ return {
 
     {
         'famiu/bufdelete.nvim',
+    },
+
+    {
+        'glepnir/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+          require('dashboard').setup { }
+        end,
+        dependencies = { {'nvim-tree/nvim-web-devicons'} }
     }
 }
