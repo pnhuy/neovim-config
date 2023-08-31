b6be0ccedc64554cef153b040a1603db10c922cc
Huy Pham <keixi512@gmail.com>
2023-08-31 15:41:15 +0700

    Add vimtex

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -248,5 +248,12 @@ return {
             vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
     
         end
-    }
+    },
+
+    {
+        'lervag/vimtex',
+        config = function()
+            vim.cmd('let g:vimtex_view_method = "sioyek"')
+        end
+    },
 }
