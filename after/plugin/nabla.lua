454dca6d9d2afee083f86a1bc11ab2cf25e5740b
Huy Pham <keixi512@gmail.com>
2023-09-30 10:25:03 +0700

    Fix nabla in unfinished latex formular

--- after/plugin/nabla.lua
+++ after/plugin/nabla.lua
@@ -1,4 +1,6 @@
 vim.api.nvim_create_autocmd({"CursorHold", "CursorHoldI"}, {
     pattern = {"*.md", "*.tex" },
-    callback = require('nabla').popup,
+    callback = function()
+        local ok, err = pcall(require('nabla').popup)
+    end,
 })
