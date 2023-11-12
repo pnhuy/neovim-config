945806852551cbd77396fd28e685133a50e33bf7
Huy Pham <keixi512@gmail.com>
2023-11-12 22:19:20 +0700

    fix clang format tab size

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -2,3 +2,9 @@
 local clangd_capabilities = vim.lsp.protocol.make_client_capabilities()
 clangd_capabilities.offsetEncoding = { "utf-16" }
 require("lspconfig").clangd.setup({ capabilities = clangd_capabilities })
+vim.cmd [[
+    let g:neoformat_cpp_clangformat = {
+        \ 'exe': 'clang-format',
+        \ 'args': ['--style="{IndentWidth: 4}"']
+    \}
+]]
